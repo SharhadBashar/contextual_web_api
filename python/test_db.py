86bf4394047d5d7877dd866d52b61862b711670e
sharhad <sharhad.bashar@uwaterloo.ca>
2023-05-10 11:12:37 -0400

    fixed bug in db write test script

--- python/test_db.py
+++ python/test_db.py
@@ -49,11 +49,11 @@ class DB:
                      Topics, TopicsMatch, Description)
                    VALUES 
                     ('3464539', '53745822', 68, 109, 546,
-                    'True', '2023-05-10 01:53:57.827971', '2023-05-10 01:53:57.827978', 
+                    'True', '{}', '{}', 
                     'Test podcast name', 'test eposide name', '[]',
                     'audio', 'https://api.spreaker.com/download/episode/53772091/20230507wkpwp_intclassic.mp3', 'https://s3.console.aws.amazon.com/s3/buckets/ts-transcription?region=us-east-1&prefix=53745822.pkl', 
                     '["wrestling", "podcast", "vip", "wwe", "week"]', '{"wrestling": {"id": 546, "data": "wrestling", "table": "Wrestling", "score": 1.0000011920928955, "count": 104}, "podcast": {"id": 371, "data": "podcast", "table": "Talk Radio", "score": 0.6291747093200684, "count": 63}, "vip": {"id": 1025, "data": "vip", "table": "Interactive", "score": 0.419993132352829, "count": 62}, "wwe": {"id": 546, "data": "wwe", "table": "Wrestling", "score": 0.8116478323936462, "count": 45}, "week": {"id": 164, "data": "week", "table": "Anniversary", "score": 0.331742525100708, "count": 43}}', 'test description')
-        """
+        """.format(datetime.now(), datetime.now())
         cursor = conn.cursor()
         cursor.execute(query)
         conn.commit()
