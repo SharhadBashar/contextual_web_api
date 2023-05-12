b2b3d72afbec65b257256a2477c630b4e68f47e9
sharhad <sharhad.bashar@uwaterloo.ca>
2023-05-11 23:27:15 -0400

    Making setup a lot easier with the ability to download specific files instead of running everything everytime

--- python/setup.py
+++ python/setup.py
@@ -1,27 +1,49 @@
 import os
+import sys
 import json
 from tqdm import tqdm
 
 from s3 import S3
 from constants import *
 
-print('Creating directories...')
-for directory in tqdm(SETUP['directory']):
+def runner():
+    print('Creating directories...')
+    for directory in tqdm(SETUP['directory']):
+        try:
+            os.mkdir(directory)
+        except:
+            None
+    print('Directories created')
+
+    f = open(os.path.join(PATH_LOG, LOG_FILENAME), 'w')
+    f.write(json.dumps(LOG_SETUP_MESSAGE))
+    f.write('\n')
+    f.close()
+
+    print('Downloading required files...')
+    s3 = S3()
+    for file, download_path in tqdm(SETUP['download'].items()):
+        s3.download_file(file, S3_CONTEXTUAL_WEB_API['name'], download_path)
+    print('Files downloaded')
+
+    print('SETUP COMLPETE')
+
+def download(file):
+    s3 = S3()
+    s3.download_file(file_name, S3_CONTEXTUAL_WEB_API['name'], SETUP['download'][file])
+    print('{} downloaded'.format(file))
+
+if __name__ == '__main__':
     try:
-        os.mkdir(directory)
-    except:
-        None
-print('Directories created')
-
-f = open(os.path.join(PATH_LOG, LOG_FILENAME), 'w')
-f.write(json.dumps(LOG_SETUP_MESSAGE))
-f.write('\n')
-f.close()
-
-print('Downloading required files...')
-s3 = S3()
-for file, download_path in tqdm(SETUP['download']):
-    s3.download_file(file, S3_CONTEXTUAL_WEB_API['name'], download_path)
-print('Files downloaded')
-
-print('SETUP COMLPETE')
+        command = sys.argv[1].lower()
+    except IndexError:
+        runner()
+        quit()
+    if (command == '-d'):
+        file_name = str(sys.argv[2].lower())
+        if (file_name not in SETUP['download'].keys()):
+            print('File name not found. Please check again')
+            quit()
+        download(file_name)
+    else:
+        print('Command not recognized. Please enter -d for download or nothing to run the entire setup')
